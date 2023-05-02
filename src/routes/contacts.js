e1c4663c9aa89c6dc8131ebbc8f1fe26e458dc8e
Jose Luis Mañon <joseluismanonrosario@gmail.com>
2023-05-02 12:15:42 -0400

    Rutas creadas del API

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const contactos=[
+    {id:1,
+    nombre: 'Jose Luis Manon',
+    correo:'manon@gmail.com',
+    telefono:'809-929-2993'
+},
+{
+    id:2,
+    nombre: 'Angel Martinez',
+    correo:'angel@gmail.com',
+    telefono:'809-923-2922'
+}
+];
+
 router.get('/', (req, res)=>{
-    res.send({
-        mensaje: 'Corriendo desde el servidor'
-    })
+    res.json(contactos)
 });
+
+router.get('/:id', (req, res)=>{
+    const {id}=req.params;
+    res.json(contactos.find(item=>item.id==id));
+});
+
+router.post('/', (req, res)=>{
+    const {id, nombre, correo, telefono}=req.body;
+    contactos.push({id, nombre, correo, telefono})
+    res.json(contactos);
+});
+
+router.put('/:id', (req, res)=>{
+    const {id}=req.params;
+    const {nombre, correo, telefono}=req.body;
+    console.log(nombre);
+    const index=contactos.findIndex(item=>item.id==id)
+    contactos[index].nombre=nombre;
+    contactos[index].correo=correo;
+    contactos[index].telefono=telefono;
+    res.json(contactos);
+})
 
 module.exports= router;
